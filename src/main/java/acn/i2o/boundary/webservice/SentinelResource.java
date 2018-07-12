c9269971e7a2564a8e8429500d7f133714dfb736
mark.g.a.penaflorida <mark.g.a.penaflorida@accenture.com>
2018-07-12 16:21:54 +0800

    i2o

--- src/main/java/acn/i2o/boundary/webservice/SentinelResource.java
+++ src/main/java/acn/i2o/boundary/webservice/SentinelResource.java
@@ -1,7 +1,11 @@
 package acn.i2o.boundary.webservice;
 
 import acn.i2o.control.service.SentinelService;
-import acn.i2o.control.valueobject.Record;
+import acn.i2o.control.valueobject.Awtomatic;
+import acn.i2o.entity.CarSeller;
+import acn.i2o.entity.Insurance;
+import acn.i2o.entity.Police;
+import acn.i2o.entity.Repairshop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +23,47 @@ public class SentinelResource {
     SentinelService sentinelService;
 
     @RequestMapping(
-            value = "/create",
+            value = "/create/carseller",
             method = RequestMethod.POST,
             consumes = "application/json")
-    public String generate(@RequestBody Record record) {
+    public String generate(@RequestBody CarSeller record) {
 
-        return sentinelService.generate(record);
+        return sentinelService.put(record);
     }
 
     @RequestMapping(
-            value = "/get",
+            value = "/create/insurance",
+            method = RequestMethod.POST,
+            consumes = "application/json")
+    public String generate(@RequestBody Insurance record) {
+
+        return sentinelService.put(record);
+    }
+
+    @RequestMapping(
+            value = "/create/police",
+            method = RequestMethod.POST,
+            consumes = "application/json")
+    public String generate(@RequestBody Police record) {
+
+        return sentinelService.put(record);
+    }
+
+    @RequestMapping(
+            value = "/create/repairshop",
+            method = RequestMethod.POST,
+            consumes = "application/json")
+    public String generate(@RequestBody Repairshop record) {
+
+        return sentinelService.put(record);
+    }
+
+    @RequestMapping(
+            value = "/awtomatic-records",
             method = RequestMethod.GET,
             produces = "application/json")
-    public List<Record> getRecords() {
+    public List<Awtomatic> getRecords() {
 
-        return sentinelService.getRecords();
+        return sentinelService.getAllAwtomaticRecords();
     }
 }
--- src/main/java/acn/i2o/control/service/SentinelService.java
+++ src/main/java/acn/i2o/control/service/SentinelService.java
@@ -1,7 +1,12 @@
 package acn.i2o.control.service;
 
 import acn.i2o.control.blockchain.HyperLedgerService;
+import acn.i2o.control.valueobject.Awtomatic;
 import acn.i2o.control.valueobject.Record;
+import acn.i2o.entity.CarSeller;
+import acn.i2o.entity.Insurance;
+import acn.i2o.entity.Police;
+import acn.i2o.entity.Repairshop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +16,27 @@ import java.util.List;
 public class SentinelService {
 
     @Autowired
-    HyperLedgerService hyperLedgerStub;
+    HyperLedgerService hyperLedgerService;
 
-    public String generate(Record record) {
-        return hyperLedgerStub.generate(record);
+    public List<Awtomatic> getAllAwtomaticRecords(){
+
+        return hyperLedgerService.awtomaticRecords();
     }
 
-    public List<Record> getRecords() {
-        return hyperLedgerStub.getRecords();
+    public String put(CarSeller carSeller){
+
+        return hyperLedgerService.insertCarSellerRecord(carSeller);
+    }
+    public String put(Insurance insurance){
+
+        return hyperLedgerService.insertInsuranceRecord(insurance);
+    }
+    public String put(Police police){
+
+        return hyperLedgerService.insertPoliceRecord(police);
+    }
+    public String put(Repairshop repairshop){
+
+        return hyperLedgerService.insertRepairshopRecord(repairshop);
     }
 }
